44e78f0c42d1cd633756ff1fbc36c7442edcb9f1
lucasalb1 <lucasalb160@gmail.com>
2023-10-09 23:27:14 -0300

    .

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,13 @@
 const img = document.querySelector( '#semaforo' );
 const buttons = document.querySelector( '#buttons' );
 
+const traffic = (event) => {
+}
+
+const turnOn = {
+    'ver': () => img.src = 'img/vermelho.png',
+    'ama': () => img.src = 'img/amarelo.png',
+    'verd': () => img.src = 'img/verde.png'
+}
+
 buttons.addEventListener('click', traffic);
