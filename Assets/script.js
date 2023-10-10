edd1a40759b206c05b4d542230533d905e1d3f67
lucasalb1 <lucasalb160@gmail.com>
2023-10-10 00:53:27 -0300

    .

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,25 @@
 const img = document.querySelector( '#semaforo' );
 const buttons = document.querySelector( '#buttons' );
+let colorIndex = 0;
 
 const traffic = (event) => {
+    turnOn[event.target.id]();
+}
+
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+const changecolor = () => {
+    const colors = ['verm', 'amar', 'verd']
+    const color = colors[ colorIndex ];
+    turnOn[color]();
+    nextIndex();
 }
 
 const turnOn = {
-    'ver': () => img.src = 'img/vermelho.png',
-    'ama': () => img.src = 'img/amarelo.png',
-    'verd': () => img.src = 'img/verde.png'
+    'verm':  () => img.src = 'img/vermelho.png',
+    'amar':  () => img.src = 'img/amarelo.png',
+    'verd': () => img.src = 'img/verde.png',
+    'auto': () => setInterval( changecolor, 1000)
 }
 
 buttons.addEventListener('click', traffic);
